0c6f8a35001ca959d79a61568a10a4de3d528885
Cai0n29 <capellakratos@gmail.com>
2024-08-27 23:14:52 +0800

    Front end

--- CountryInfo.py
+++ CountryInfo.py
@@ -1,7 +1,7 @@
+import streamlit as st
 import pandas as pd 
-country = pd.read_csv(r"C:\Users\jto07\Python-projects\countries.csv",)
+country = pd.read_csv(r"C:\Users\jto07\OneDrive\Desktop\HOOK\hook\Mundo\countries.csv")
 country = country.drop(columns = "Pop. Density (per sq. mi.)")
-country = country.drop(columns = "Area (sq. mi.)")
 country = country.drop(columns = "Other (%)")
 country = country.drop(columns = "Net migration")
 country = country.drop(columns = "Infant mortality (per 1000 births)")
@@ -9,17 +9,20 @@ country = country.drop(columns = "Literacy (%)")
 country = country.drop(columns = "Phones (per 1000)")
 country = country.drop(columns = "Arable (%)")
 country = country.drop(columns = "Crops (%)")
-country = country.drop(columns = "Climate")
 country = country.drop(columns = "Birthrate")
 country = country.drop(columns = "Deathrate")
 country = country.drop(columns = "Agriculture")
 country = country.drop(columns = "Industry")
 country = country.drop(columns = "Service")
-print('What country do you like to know?')
-coun = input() + ' '
+st.title('Mundo 🌎')
+count = st.text_input('Input a Country')
+coun = count + ' '
 index = country.index[country['Country']==coun].tolist()
 info = country.loc[index]
-if index == []:
-    print('The country do not exist')
+if count == '':
+    st.write('')
 else:
-    print(info)
+    if index == []:
+        st.write('Kindly input a name of a country again')
+    else:
+        st.write(info)
